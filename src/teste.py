e7dae1311d08b4e773e943ab0c1b7ef4d6dcf03c
Wilkerpy <wilson.insfran@aluno.faculdadeimpacta.com.br>
2020-09-29 21:54:17 -0300

    correcao3

--- src/teste.py
+++ src/teste.py
@@ -1,3 +1,4 @@
+
 import jogovelha
 import sys
 
@@ -5,16 +6,16 @@ erroInicializar = False
 jogo = jogovelha.inicializar()
 
 if len(jogo) != 3:
-    erroInicializar = True
+	erroInicializar = True
 else:
-    for linha in jogo:
-        if len(linha) != 3:
-            erroInicializar = True
-        else:
-            for elemento in linha:
-                if elemento != '.':
-                    erroInicializar = True
+	for linha in jogo:
+		if len(linha) != 3:
+			erroInicializar = True
+		else:
+			for elemento in linha:
+				if elemento != '.':
+					erroInicializar = True
 if erroInicializar:
-    sys.exit(1)
+	sys.exit(1)
 else:
-    sys.exit(0)
+	sys.exit(0)
